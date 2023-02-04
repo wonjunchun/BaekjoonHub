fc51a1bcff7bb1440535c3ad65b305c265ebd6a8
원준 <chunjh1103@naver.com>
2023-02-04 23:40:38 +0900

    [Silver III] Title: N과 M (6), Time: 232 ms, Memory: 18356 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * 백트래킹
+ */
+public class Main {
+    static List<List<Integer>> sequences = new ArrayList<>(); //수열들의 목록
+    static List<Integer> numlist = new ArrayList<>();//
+    public static void solve(int N, int M, int current, List<Integer> seq){
+        if(M == 0){ //수열의 길이에 도달하였다면
+            //더이상 seq에 추가안하고 현재 수열 sequences에 넣고 종료
+            sequences.add(seq);
+            return;
+        }
+        for(int i: numlist) {
+            //수열은 사전순으로 증가하는 순서로만 올수 있으므로
+            //현재 값보다 큰값만 다음 원소로 seq에 넣어야함
+            if(i <= current) continue;
+
+            List<Integer> newSeq = new ArrayList<>();
+            newSeq.addAll(seq);
+            newSeq.add(i);
+            solve(N, M-1, i, newSeq); //현재 수열에 앞으로 M-1개 만큼 더 넣음
+        }
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        for(int i = 0; i < N; i++){
+            numlist.add(sc.nextInt());
+        }
+        numlist.sort(Comparator.naturalOrder()); //numlist 오름차순 정렬
+        for(int n: numlist){
+            List<Integer> seq = new ArrayList<>();
+            seq.add(n);
+            solve(N, M-1, n, seq);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(List<Integer> s: sequences){
+            for(int nums: s){
+                sb.append(nums+" ");
+            }
+            sb.append("\n");
+        }
+        System.out.printf(sb.toString());
+    }
+}
