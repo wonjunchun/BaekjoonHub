880b26a6a8973f9c03484c5e70e016724af0a11e
원준 <chunjh1103@naver.com>
2024-02-03 11:32:10 +0900

    [Bronze V] Title: 새싹, Time: 72 ms, Memory: 11204 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,18 @@
+public class Main{
+    public static void main(String[] args){
+        System.out.println("         ,r\'\"7");
+        System.out.println("r`-_   ,\'  ,/");
+        System.out.println(" \\. \". L_r\'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+    }
+}
+/*
+         ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      |
+*/
