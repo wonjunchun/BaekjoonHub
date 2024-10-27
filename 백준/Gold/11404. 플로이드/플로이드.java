10dbd6e95864769dad7201a73769201351fd2816
원준 <chunjh1103@naver.com>
2024-10-27 17:30:10 +0900

    [Gold IV] Title: 플로이드, Time: 392 ms, Memory: 43764 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -1,49 +1,55 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[][] adjMatrix;
-    static int N, M;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        M = Integer.parseInt(br.readLine());
-        adjMatrix = new int[N + 1][N + 1];
-        //초기화
-        for(int n = 1; n <= N; n++){
-            Arrays.fill(adjMatrix[n], Integer.MAX_VALUE);
-//            adjMatrix[n][n] = 0;
-        }
         StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N+1][N+1];
+
+        //초기화
+        for(int i = 1; i <= N; i++){
+            for(int j = 1; j <= N; j++){
+                arr[i][j] = Integer.MAX_VALUE;
+                if(i == j) arr[i][j] = 0;
+            }
+        }
+
         for(int m = 0; m < M; m++){
             st = new StringTokenizer(br.readLine());
             int src = Integer.parseInt(st.nextToken());
             int dst = Integer.parseInt(st.nextToken());
             int cost = Integer.parseInt(st.nextToken());
-            if(adjMatrix[src][dst] > cost)
-                adjMatrix[src][dst] = cost;
-        }
-        floyd();
-        for(int i = 1; i <= N; i++){
-            for(int j = 1; j <= N; j++){
-                int result = (i == j || adjMatrix[i][j] == Integer.MAX_VALUE) ? 0 : adjMatrix[i][j];
-                System.out.print(result + " ");
-            }
-            System.out.println();
-        }
-    }
 
-    static void floyd() {
+            arr[src][dst] = Math.min(arr[src][dst], cost); //최소값으로 갱신
+        }
+
+        //플로이드워셜
         for(int k = 1; k <= N; k++){
             for(int i = 1; i <= N; i++){
                 for(int j = 1; j <= N; j++){
-                    if(adjMatrix[i][k] != Integer.MAX_VALUE && adjMatrix[k][j] != Integer.MAX_VALUE)
-                        adjMatrix[i][j] = Math.min(adjMatrix[i][j], adjMatrix[i][k] + adjMatrix[k][j]);
+                    if(arr[i][k] != Integer.MAX_VALUE && arr[k][j] != Integer.MAX_VALUE
+                            && arr[i][j] > arr[i][k] + arr[k][j]){
+                        arr[i][j] = arr[i][k] + arr[k][j]; //최단경로로 갱신
+                    }
                 }
             }
         }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i <= N; i++){
+            for(int j = 1; j <= N; j++){
+                if(arr[i][j] == Integer.MAX_VALUE)
+                    arr[i][j] = 0;
+                sb.append(arr[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb.toString());
     }
 }
